e2078cd9726c81f02e0c6dfc2e23ce24c3b4cb21
Alberto Montes <alm59321@gmail.com>
2018-05-16 10:01:00 +0200

    ✨ Updated results analysis

--- analyze_report.py
+++ analyze_report.py
@@ -1,9 +1,30 @@
-import pandas as pd
+""" Analyse Global Summary
+"""
+import json
+
 import matplotlib.pyplot as plt
 
-# Plot the evolution of the IoU with the number of interactions
-df = pd.read_csv('results/result_default_settings.csv', index_col=0)
-df.groupby('interaction').jaccard.mean().plot()
-plt.ylabel('IoU')
-plt.xlabel('Number of Interactions')
-plt.show()
+
+def main():
+    with open('results/summary.json', 'r') as fp:
+        summary = json.load(fp)
+
+    print('AUC: \t{:.3f}'.format(summary['auc']))
+    th = summary['jaccard_at_threshold']['threshold']
+    jac = summary['jaccard_at_threshold']['jaccard']
+    print('J@{}: \t{:.3f}'.format(th, jac))
+
+    time = summary['curve']['time']
+    jaccard = summary['curve']['jaccard']
+
+    plt.plot(time, jaccard)
+    plt.ylim([0, 1])
+    plt.xlim([0, max(time)])
+    plt.xlabel('Accumulated Time (s)')
+    plt.ylabel(r'Jaccard ($\mathcal{J}$)')
+    plt.axvline(th, c='k')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
